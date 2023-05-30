f6036ab7e43e352eda414de980d4aaf5f67b5ff3
G. Yang <107653576+gyang0@users.noreply.github.com>
2023-05-30 04:48:09 -0700

    Create main.js
    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+var STATS = (function(){
+    function mean(arr){
+        var sum = 0;
+        for(var i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        return sum/arr.length;
+    }
+    
+    function median(arr){
+    		arr.sort();
+        if(arr.length % 2 == 1){
+        		return arr[Math.floor(arr.length/2)];
+        }
+        else {
+        		return (arr[arr.length/2 - 1] + arr[arr.length/2])/2;
+        }
+    }
+    
+    function mode(arr){}
+    
+    function stdDeviation(arr){}
+    
+    function variance(arr){}
+    
+    function confidenceInterval(arr){}
+    
+    return {
+    		mean: mean,
+        mode: mode,
+        median: median,
+        stdDeviation: stdDeviation,
+        variance: variance,
+        confidenceInterval: confidenceInterval
+    };
+})();
