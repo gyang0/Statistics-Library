29e4046839b7481af0da86450571a2d53ccc6729
G. Yang <107653576+gyang0@users.noreply.github.com>
2023-06-06 01:30:11 -0700

    Add PMCC and linear regression for scatterplots

--- main.js
+++ main.js
@@ -6,17 +6,17 @@
  * 
  * List of methods:
  * Constants
- *     z-score for 95% confidence
- *     z-score for 99% confidence
+ *     [DONE] z-score for 95% confidence
+ *     [DONE] z-score for 99% confidence
  * 
  * Basics
- *     Mean
- *     Median
- *     Mode
- *     Variance
- *     Standard Deviation
- *     Confidence intervals
- *     Confidence intervals for mean differences
+ *     [DONE] Mean
+ *     [DONE] Median
+ *     [DONE] Mode
+ *     [DONE] Variance
+ *     [DONE] Standard Deviation
+ *     [DONE] Confidence intervals
+ *     [DONE] Confidence intervals for mean differences
  * 
  * Tests
  *     Two-sample t-test
@@ -26,7 +26,7 @@
  *     ANOVA (analysis of variance)
  * 
  * Correlation
- *     PMCC (product-moment correlation coefficient)
+ *     [DONE] PMCC (product-moment correlation coefficient)
  *     SRCC (spearman-rank correlation coefficient)
  *     Chi-square test for goodness of fit
  *     Chi-square test for association
@@ -40,7 +40,7 @@
  * 
  * 
  * @author Gene Yang
- * @version June 4th, 2023
+ * @version June 6th, 2023
  */ 
 var STATS = (function(){
     /**
@@ -79,7 +79,6 @@ var STATS = (function(){
         }
     }
     
-
     /**
      * Finds the mode of a specified array
      * @param arr - The array to find the mode of
@@ -133,10 +132,10 @@ var STATS = (function(){
     /**
      * Returns the sample variance of a dataset
      * @param arr - The dataset
-     * @return The sample variance of the dataset
+     * @return - The sample variance of the dataset
      */ 
     function variance(arr){
-        var xbar = this.mean(arr),
+        var xbar = mean(arr),
             sum = 0;
         for(var i = 0; i < arr.length; i++){
             sum += (xbar - arr[i])*(xbar - arr[i]);
@@ -148,17 +147,17 @@ var STATS = (function(){
     /**
      * Returns the sample standard deviation of a specified dataset.
      * @param arr - The dataset of numbers
-     * @return the standard deviation of a dataset (square root of sample variance)
+     * @return - The standard deviation of a dataset (square root of sample variance)
      */
     function stdDev(arr){
-        return Math.sqrt(this.variance(arr));
+        return Math.sqrt(variance(arr));
     }
     
     /**
      * Finds the sample confidence interval of a dataset.
      * @param arr - The dataset
      * @param confidence - Either 95 or 99, the default is 95.
-     * @return An array of two elements: the lower bound and the upper bound
+     * @return - An array of two elements: the lower bound and the upper bound
      */ 
     function confidenceInterval(arr, confidence){
         var xbar = this.mean(arr);
@@ -181,9 +180,9 @@ var STATS = (function(){
      * @param arr1 - Dataset #1
      * @param arr2 - Dataset #2
      * @param confidence - Either 95 or 99, the default is 95.
-     * @return An array of two elements: the lower bound and the upper bound
+     * @return - An array of two elements: the lower bound and the upper bound
      */ 
-    function CFMeanDifference(arr1, arr2, confidence){
+    function CIMeanDifference(arr1, arr2, confidence){
         var arg1 = (this.mean(arr1) - this.mean(arr2));
         var arg2;
         if(confidence == 99)
@@ -199,9 +198,43 @@ var STATS = (function(){
         return [arg1 - arg2, arg1 + arg2];
     }
 
-    function pmcc(){}
+    /**
+     * Finds the Product Moment Correlation Coefficient of a dataset.
+     * Applicable to interval and ratio scale data.
+     * @param data - An array of 2D elements in the form [x, y], e,g, [[x1, y1], [x2, y2], [x3, y3]...]
+     * @return - The correlation coefficient, a number in the range [-1, 1].
+     */
+    function pmcc(data){
+        // Get means of x and y observations
+        var xBar = 0,
+            yBar = 0;
+        for(var i = 0; i < data.length; i++){
+            xBar += data[i][0];
+            yBar += data[i][1];
+        }
+        xBar /= data.length;
+        yBar /= data.length;
 
-    function srcc(){}
+        var sum0 = 0,
+            sum1 = 0,
+            sum2 = 0;
+
+        for(var i = 0; i < data.length; i++){
+            sum0 += (data[i][0] - xBar) * (data[i][1] - yBar);
+            sum1 += (data[i][0] - xBar) * (data[i][0] - xBar);
+            sum2 += (data[i][1] - yBar) * (data[i][1] - yBar);
+        }
+
+        return sum0/Math.sqrt(sum1 * sum2);
+    }
+    
+    /**
+     * Finds the Spearman Rank Correlation Coefficient of a dataset.
+     * Applicable to ordinal, interval, and ratio scale data.
+     * @param data - An array of 2D elements in the form [x, y], e,g, [[x1, y1], [x2, y2], [x3, y3]...]
+     * @return - The correlation coefficient, a number in the range [-1, 1].
+     */
+    function srcc(data){}
 
     function chiSquareGoodnessOfFit(){}
 
@@ -257,7 +290,23 @@ var STATS = (function(){
                 this.data = data;
             },
             draw: function(ctx){
-                // TODO: Add titles for entire scatterplot, x-axis, and y-axis.
+                // Scatterplot title
+                ctx.font = "20px serif";
+                ctx.fillStyle = "black";
+                ctx.textAlign = "center";
+                ctx.fillText(this.title, this.x + this.xSpacing*this.numXTicks/2, this.y - this.ySpacing*(this.numYTicks + 1));
+
+                // x-axis title
+                ctx.font = "17px serif";
+                ctx.fillText(this.xTitle, this.x+ this.xSpacing*this.numXTicks/2, this.y + this.ySpacing);
+
+                // y-axis title
+                ctx.save();
+                ctx.translate(this.x - this.xSpacing, this.y - this.ySpacing*this.numYTicks/2);
+                ctx.rotate(3 * Math.PI/2);
+                ctx.fillText(this.yTitle, 0, 0);
+                ctx.translate(-(this.x - this.xSpacing), -(this.y - this.ySpacing*this.numYTicks/2));
+                ctx.restore();
 
                 // Axes
                 drawLine(ctx, this.x, this.y, this.x, this.y - this.ySpacing*this.numYTicks); // y-axis
@@ -286,8 +335,87 @@ var STATS = (function(){
                     ctx.stroke();
                 }
             },
-            lineOfBestFit: function(){
-                // TODO: Line of best fit goes here
+            lineOfBestFit: function(type, ctx){
+                // Simple linear regression
+                if(type == "simple"){
+                    var sumX = 0,
+                        sumY = 0,
+                        sumXY = 0,
+                        sumXSquared = 0,
+                        n = this.data.length;
+
+                    for(var i = 0; i < this.data.length; i++){
+                        sumX += this.data[i][0];
+                        sumY += this.data[i][1];
+                        sumXY += (this.data[i][0] * this.data[i][1]);
+                        sumXSquared += (this.data[i][0] * this.data[i][0]);
+                    }
+
+                    // Mean center and slope
+                    var slope = (n*sumXY - sumX*sumY)/(n*sumXSquared - sumX*sumX);
+                    var meanCenter = [sumX/n, sumY/n];
+
+                    // Equation form y = ax + b
+                    // y - y1 = slope * (x - x1)
+                    // y - meanCenter[1] = slope * (x - meanCenter[0])
+                    // y - meanCenter[1] = slope*x - slope*meanCenter[0]
+                    // y = slope*x + (-slope*meanCenter[0] + meanCenter[1])
+                    var a = slope,
+                        b = (-slope*meanCenter[0] + meanCenter[1])
+
+                    // Draws the line on the graph.
+                    // Using the equation y = ax + b, the first point on the graph is when x = this.xStartNum
+                    //                                the second point is when x = this.xStartNum + this.numXTicks
+                    ctx.fillStyle = "black";
+                    var x1 = this.xStartNum,
+                        x2 = this.xStartNum + this.numXTicks;
+                    drawLine(ctx, this.x + this.xSpacing*x1, this.y - this.ySpacing*(a*x1 + b),
+                                  this.x + this.xSpacing*x2, this.y - this.ySpacing*(a*x2 + b));
+
+
+                    // Returns information needed for equation of the line as [slope, y-intercept]
+                    return [a, b];
+                }
+
+                // Model 2 linear regression
+                else {
+                    // Warning message
+                    if(type != "model2")
+                        console.log("STATS.lineOfBestFit(): unrecognized type parameter, defaulted to 'model2.'");
+
+                    // y = ax + b
+                    var x = [],
+                        y = [],
+                        sumX = 0,
+                        sumY = 0;
+
+                    for(var i = 0; i < this.data.length; i++){
+                        x.push(this.data[i][0]);
+                        y.push(this.data[i][1]);
+                        sumX += this.data[i][0];
+                        sumY += this.data[i][1];
+                    }
+
+                    // It's either a positive or negative correlation (+- slope, we just have to know which one).
+                    slope = stdDev(x)/stdDev(y);
+                    slope *= (pmcc(this.data) < 0 ? -1 : 1);
+
+                    // Slope and mean center
+                    var meanCenter = [mean(x), mean(y)];
+                    var a = slope,
+                        b = (-slope*meanCenter[0] + meanCenter[1])
+
+                    // Draws the line on the graph.
+                    ctx.fillStyle = "black";
+                    var x1 = this.xStartNum,
+                        x2 = this.xStartNum + this.numXTicks;
+                    drawLine(ctx, this.x + this.xSpacing*x1, this.y - this.ySpacing*(a*x1 + b),
+                                  this.x + this.xSpacing*x2, this.y - this.ySpacing*(a*x2 + b));
+
+
+                    // Returns information needed for equation of the line as [slope, y-intercept]
+                    return [a, b];
+                }
             }
         };
 
@@ -305,7 +433,8 @@ var STATS = (function(){
         lineGraph.prototype = {
             addData: function(data){
                 this.data = data;
-            }
+            },
+            draw: function(data){}
         };
     })();
 
@@ -351,7 +480,7 @@ var STATS = (function(){
         stdDev: stdDev,
         variance: variance,
         confidenceInterval: confidenceInterval,
-        CFMeanDifference: CFMeanDifference,
+        CIMeanDifference: CIMeanDifference,
 
         pmcc: pmcc,
         srcc: srcc,
